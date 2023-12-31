d764eca08ec43ab75b1d701d92bc53b769737457
Alson <alson.a@disprz.com>
2023-12-31 19:37:47 +0530

    App title changes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const path = require('path');
 const port = process.env.PORT || 5000;
 const colors = require('colors');
 const connectDB = require('../backend/config/db')
@@ -14,6 +15,20 @@ app.use(express.urlencoded({extended: false}));
 
 app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(
+        path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+      )
+    );
+  } else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+  }
+  
 
 app.use(errorHandler);
 
